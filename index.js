de08800cb90f55a1db89f2c2b8b6582e063e0194
Vyshnav <vyshnavthaithottathil@gmail.com>
2023-04-26 18:14:42 +0530

    fix togglebar&pages

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let resume=document.getElementById("rbutton");
 
 
 contact.addEventListener("click",()=>{
-    window.location.href="http://127.0.0.1:5500/contact.html"
+    window.location.href="/contact.html"
 })
 resume.addEventListener("click",()=>{
     window.location.href="https://drive.google.com/drive/u/0/my-drive"
