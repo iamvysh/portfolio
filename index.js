58e3ef9c9d729c32974279435dd687252cf37183
Vyshnav <vyshnavthaithottathil@gmail.com>
2023-04-27 12:44:34 +0530

    Fix pages and contact shortcut

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let resume=document.getElementById("rbutton");
 
 
 contact.addEventListener("click",()=>{
-    window.location.href="/contact.html"
+    window.location.href="./contact.html"
 })
 resume.addEventListener("click",()=>{
     window.location.href="https://drive.google.com/file/d/1NCLcDgMxRbp0OeNTZqJ8G0_mFqVYbW6B/view?usp=drivesdk"
