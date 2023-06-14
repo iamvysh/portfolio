7f33e9b04bea8abc0c7387f414aef1ba15ed4730
iamvysh <vyshnavthaithottathil@gmail.com>
2023-06-14 10:45:07 +0530

    ad

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ contact.addEventListener("click",()=>{
     window.location.href="./contact.html"
 })
 resume.addEventListener("click",()=>{
-    window.location.href="https://drive.google.com/file/d/1NCLcDgMxRbp0OeNTZqJ8G0_mFqVYbW6B/view?usp=drivesdk"
+    window.location.href="https://drive.google.com/file/d/1NCLcDgMxRbp0OeNTZqJ8G0_mFqVYbW6B/view"
 })
